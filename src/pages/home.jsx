1225ec174c26cb1333477dcd52120db896cf2d5d
MalshaniBandaranayake <malshanibandaranayake91@gmail.com>
2024-04-06 10:07:26 +0530

    update patient home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,20 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import './MainHome.css'; // Import CSS file
 
  function home() {
   return (
     <div>
       <Navbar/>
-      <h1>home page</h1>
+      <h2>Home Page</h2>
+
+      <div className="imageContainer">
+        <div className="image image1"></div>
+        <div className="image image2"></div>
+        <div className="image image3"></div>
+        <div className="image image4"></div>
+      </div>
       
       <Footer /> 
     </div>
